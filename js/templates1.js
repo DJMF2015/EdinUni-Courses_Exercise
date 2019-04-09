f46436d8a8cc565d22a9a016c5b56c4fba5643cd
DJMF2015 <djm.fulton@gmail.com>
2019-04-09 13:13:42 +0100

    add in overall eighted mark for both two modules. Still to dd in bootstrap colour coded talbe to display grade based upon mark

--- js/templates1.js
+++ js/templates1.js
@@ -1,24 +1,29 @@
 const assessmentTemplate = function(assessment) {
-  console.log(assessment.name)
+  // console.log(assessment.name)
   //display weighting per course , course name and mark available
   return `
   <tr>
   <td>${assessment.name}</td>
   <td>${assessment.weight.toPrecision(4) * 100} + '%' </td>
   <td>${assessment.mark}</td>
-  </tr>  `;
+  <td>${calculateGradeHelper(assessment.mark)}</td>
+  </tr>
+
+  `
 }
 
+//TO-DO to display table wih colour codes
+const calculateGradeHelper = function(mark){
+  //use Bootstrap 3 .label class,
+}
 
-//  2. Display the topics per course. topics for Each
+// 2. Display the topics per course. topics for Each
 // Check if there are no assessments.
 const assessmentTemplateHelper = function(course){
   let result = ''
-  if (course.assessments.length === 0) return `<tr colspan="100%"><td>No assessments found.</td></tr>`;
+  if (course.assessments.length === 0) return `<tr colspan="100%"><td>No assessments found.</td></tr></tabel>`;
   for (let assess of course.assessments){
     courses += course.readingList
-
-    // console.log(courses)
     result += assessmentTemplate(assess)
     // readingListTemplateHelper(course)
   }
@@ -34,6 +39,7 @@ const courseAndReadingListTemplate = function(course) {
   `
 
 }
+
 // const sortReadingList = function (output){
 //   let readA = ''
 //   console.log(output)
@@ -53,22 +59,9 @@ const readingListTemplateHelper = function(course){
   return output
 }
 
-const sortList = function(course){
-  let array = []
-  for (value of course.readingList){
-    array.push(course.readingList)
-    console.log(array)
-    if (value.image === null){
-      value.image = '404.png';
-    }
-  }
-  console.log(array)
-  return array
-}
-
 const readingListTemplate = function(read) {
 
-  //3 list readinglist in ascending order by date due. fix image
+  //3 TO DOlist readinglist in ascending order by date due.
   return `
   <div class="reading-list-item">
   <p><b>${read.title}</b></p>
@@ -80,14 +73,37 @@ const readingListTemplate = function(read) {
   `;
 }
 
-const finalGradeMarkHelper = function(course){
-  const percentagesArray = arrayOfPercentages(course)
-  const finalMark = calculateFInalMark(percentagesArray)
-  return finalMark
-  
-}
 //4. Show grades based on the marks.
 //display weighting per course , course name and mark available
+const arrayOfPercentages = function(course){
+  let percentage = []
+  for (index of course.assessments){
+    let percentagemark = (index.mark / 100) //75
+    let percentageweighting =  percentagemark * index.weight  //0.333
+    // console.log(percentageweighting) //0.45
+    let overallmark = percentageweighting//"0.63", "0.11", "0.06"]
+    percentage.push(overallmark)
+
+  }
+  return percentage
+}
+
+const calculateOverallMark = function (percentages){
+  //total all weighted percentages
+  let total = 0
+  for (percent of percentages){
+    total += percent
+  }
+  return total * 100
+}
+
+
+const finalGradeHelper = function(course){
+  const percentagesArray = arrayOfPercentages(course)
+  let finalMark = calculateOverallMark(percentagesArray);
+  return `<h4>Final Mark: ${finalMark.toPrecision(2)}</h4>  `
+
+}
 
 //5. Calculate and display the overall course mark.
 //Add in the overall grade to be shown next to the overall final course mark
@@ -108,7 +124,8 @@ const courseTemplate = function(course) {
   </thead>
   <tbody>
   ${assessmentTemplateHelper(course)}
-  ${finalGradeMark(course)}
+  ${finalGradeHelper(course)}
+
   </tbody>
   </table>
   `
